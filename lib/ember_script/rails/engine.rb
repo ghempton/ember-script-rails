0d76440d0a89c09d0962c174da278b5e36ce5774
Tony Novak <tony@amitree.com>
2016-01-15 14:46:32 -0500

    Passing mime_type to fix encoding issues

--- lib/ember_script/rails/engine.rb
+++ lib/ember_script/rails/engine.rb
@@ -5,8 +5,8 @@ module EmberScript
 
       initializer "ember_script.setup", :after => :'load_environment_config', :group => :all do |app|
         app.config.assets.configure do |env|
-          env.register_engine '.em', EmberScript::EmberScriptTemplate
-          env.register_engine '.js.em', EmberScript::EmberScriptTemplate
+          env.register_engine '.em', EmberScript::EmberScriptTemplate, mime_type: 'application/javascript'
+          env.register_engine '.js.em', EmberScript::EmberScriptTemplate, mime_type: 'application/javascript'
         end
       end
     end
